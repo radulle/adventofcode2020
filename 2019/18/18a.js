5f4f8a0a69dafdefd38b0dff46e0eb19bd9a9f7d
radulle <radulle@gmail.com>
2020-12-14 12:28:00 +0100

    WIP: add 2020-18

---
+++ 2019/18/18a.js
@@ -0,0 +1,150 @@
+const fs = require("fs")
+const Graph = require("./graph")
+
+global.console.infoTime = (logMe) => {
+  console.time()
+  console.info(typeof logMe === "function" ? logMe() : logMe)
+  console.timeEnd()
+}
+
+const neighbors = [
+  [1, 0],
+  [0, 1],
+  [-1, 0],
+  [0, -1],
+]
+
+const grid = fs
+  .readFileSync("data.txt", "utf8")
+  .split("\r\n")
+  .map((row) => row.split(""))
+
+const graph = new Graph()
+
+const edges = []
+const locked = []
+for (let i = 1; i < grid.length - 1; i++) {
+  for (let j = 1; j < grid[i].length - 1; j++) {
+    gridToGraph(edges, grid, i, j)
+  }
+}
+edges.forEach(([A, B]) => graph.addEdge(A, B))
+locked.forEach(([A, B]) => graph.addLocked(A, B))
+
+function gridToGraph(edges, grid, i, j) {
+  const type = grid[i][j]
+  const bit =
+    (/[a-z]/.test(type) &&
+      Math.pow(2, type.charCodeAt(0) - "a".charCodeAt(0))) ||
+    0
+  if (type === "#") return
+  graph.addNode([i, j], type, { bit })
+  neighbors.forEach(([y, x]) => {
+    if (grid[i + y][j + x] !== "#")
+      if (/[A-Z]/.test(type)) {
+        locked.push([
+          [i, j],
+          [i + y, j + x],
+        ])
+      } else {
+        edges.push([
+          [i, j],
+          [i + y, j + x],
+        ])
+      }
+  })
+}
+
+class Scout {
+  keys = new Set()
+
+  search(graph, sourceNode) {
+    const finishers = []
+    const cache = new Map()
+
+    function getReachableKeys(graph, sourceKey) {
+      const reachable = graph.bfs(sourceKey)
+      return graph.getKeyNodes().reduce((acc, cur) => {
+        const steps = reachable.get(cur.key)
+        if (steps) acc.push({ ...cur, steps })
+        return acc
+      }, [])
+    }
+
+    function getReachables(graph, sourceKey) {
+      const reachable = graph.bfs(sourceKey)
+      return graph.getDistinctNodes().reduce((acc, cur) => {
+        const steps = reachable.get(cur.key)
+        if (steps) acc.push({ ...cur, steps })
+        return acc
+      }, [])
+    }
+
+    const search = (
+      graph,
+      { key, type, steps = 0, node, bit },
+      path = [0, 0],
+      recursionIdx = 0
+    ) => {
+      // console.info(graph.draw([{ node, type: "X" }]))
+      // console.info(
+      //   [path[0].toString(2), path[1]],
+      //   [...cache].map((e) => [
+      //     e[0].toString(2),
+      //     // .split("")
+      //     // .map((c) => String.fromCharCode("a".charCodeAt(0) + c * Number(c))),
+      //     e[1],
+      //   ])
+      // )
+      // console.info(recursionIdx)
+      if (recursionIdx > 26) return
+      if (
+        [...cache].some(
+          (cached) => (cached[0] & path[0]) === path[0] && cached[1] < path[1]
+        )
+      )
+        return
+      cache.set(...path)
+
+      const currentGraph = graph.copy()
+      const current = currentGraph.getNodeByKey(key)
+
+      current.type = "."
+      const door = currentGraph.getNodeByType(type.toUpperCase())
+      // console.info(door)
+      if (door) {
+        door.edges = door.locked
+        delete door.locked
+        door.type = "."
+      }
+      // console.info(door)
+
+      const reachables = getReachableKeys(currentGraph, key).sort((a,b) => b.steps - a.steps)
+
+      if (!currentGraph.getKeyNodes().length) return finishers.push(path)
+      if (!reachables.length) return
+
+      for (let reachable of reachables) {
+        const nodePath = [path[0] + reachable.bit, path[1] + reachable.steps]
+        search(currentGraph, reachable, nodePath, recursionIdx + 1)
+      }
+    }
+    search(graph, sourceNode)
+    return finishers
+  }
+}
+
+const scout = new Scout()
+const result = scout.search(graph, graph.getNodeByType("@"))
+console.info(
+  // result,
+  result
+  // .map((route) =>
+  //   route.reduce((acc, { steps }) => {
+  //     return acc + steps
+  //   }, 0)
+  // )
+  .sort((a, b) => a[1] - b[1])
+)
+
+// 3944 too high
---
+++ 2019/18/graph.js
@@ -0,0 +1,134 @@
+module.exports = class Graph {
+  constructor(adjacent = {}) {
+    this.adjacent = adjacent
+  }
+
+  /** Deep copy of essential data into new Graph */
+  copy() {
+    return new Graph(
+      Object.fromEntries(
+        Object.entries(this.adjacent).map(
+          ([key, { node, edges, locked, ...rest }]) => [
+            key,
+            {
+              key,
+              node: typeof node === "string" ? node : [...node],
+              edges: new Set(edges),
+              locked: new Set(locked),
+              ...rest,
+            },
+          ]
+        )
+      )
+    )
+  }
+
+  getNodeByKey(key) {
+    return this.adjacent[key]
+  }
+
+  getNodeByType(type) {
+    return Object.values(this.adjacent).find((val) => val.type === type)
+  }
+
+  getDistinctNodes() {
+    return Object.values(this.adjacent).filter((val) => val.type !== ".")
+  }
+
+  getKeyNodes() {
+    return Object.values(this.adjacent).filter((val) => /[a-z]/.test(val.type))
+  }
+
+  getDoorNodes() {
+    return Object.values(this.adjacent).filter((val) => /[A-Z]/.test(val.type))
+  }
+
+  addNode(node, type, extra) {
+    const key = toKey(node)
+    if (this.adjacent[key]) throw new Error("Node already exists!")
+    this.adjacent[key] = {
+      key,
+      node,
+      type,
+      edges: new Set(),
+      locked: new Set(),
+      ...extra,
+    }
+  }
+
+  addEdge(A, B) {
+    if (!this.adjacent[toKey(A)] || !this.adjacent[toKey(B)]) {
+      throw new Error("Missing vertices!")
+    }
+    this.adjacent[toKey(A)].edges.add(toKey(B))
+  }
+
+  addLocked(A, B) {
+    if (!this.adjacent[toKey(A)] || !this.adjacent[toKey(B)]) {
+      throw new Error("Missing vertices!")
+    }
+    this.adjacent[toKey(A)].locked.add(toKey(B))
+  }
+
+  /** Breadth first search */
+  bfs(source) {
+    const visited = new Map()
+    let que = []
+
+    visited.set(toKey(source), 0)
+    que.push([toKey(source), 0])
+
+    while (que.length) {
+      const [container, step] = que.pop()
+      for (let node of this.adjacent[container].edges) {
+        if (!visited.has(node)) {
+          visited.set(node, step + 1)
+          que.unshift([node, step + 1])
+        }
+      }
+    }
+    return visited
+  }
+
+  /** Depth first search */
+  dfs(source) {
+    const visited = new Map()
+
+    const dfs = (source, visited, step = 0) => {
+      visited.set(source, step)
+      for (let node of this.adjacent[source].edges) {
+        if (!visited.has(node)) {
+          dfs(node, visited, step + 1)
+        }
+      }
+    }
+
+    dfs(toKey(source), visited)
+    return visited
+  }
+
+  /** Draws if each node have a node [row, col] */
+  draw(overlay = []) {
+    const size = Object.values(this.adjacent).reduce(
+      (acc, cur) => {
+        if (cur.node[0] > acc[0]) acc[0] = cur.node[0]
+        if (cur.node[1] > acc[1]) acc[1] = cur.node[1]
+        return acc
+      },
+      [0, 0]
+    )
+    const canvas = new Array(size[0] + 2)
+      .fill(null)
+      .map(() => new Array(size[1] + 2).fill("#"))
+
+    const draw = ({ node: [row, col], type }) => {
+      canvas[row][col] = type
+    }
+
+    Object.values(this.adjacent).forEach(draw)
+    overlay.forEach(draw)
+    return canvas.map((row) => row.join("")).join("\n")
+  }
+}
+
+const toKey = (v) => (Array.isArray(v) ? `${v[0]},${v[1]}` : v)
