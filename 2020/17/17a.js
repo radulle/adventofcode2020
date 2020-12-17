5364fc02de763ef3e650b79babce5264b8c5b590
radulle <radulle@gmail.com>
2020-12-17 17:51:40 +0100

    add 2020-17

---
+++ 2020/17/17a.js
@@ -0,0 +1,62 @@
+const fs = require("fs")
+
+let data = fs
+  .readFileSync("data.txt", "utf8")
+  .split("\r\n")
+  .map((row) => row.split("").map((e) => (e === "#" ? 1 : 0)))
+
+const cycles = 6
+const size = data.length + 2 * cycles + 2
+const makeCube = (size) =>
+  new Array(size)
+    .fill(null)
+    .map(() => new Array(size).fill(null).map(() => new Array(size).fill(0)))
+
+let cube = makeCube(size)
+data.forEach((row, j) =>
+  row.forEach(
+    (cell, k) =>
+      (cube[size / 2][size / 2 - data.length / 2 + j][
+        size / 2 - data.length / 2 + k
+      ] = cell)
+  )
+)
+
+for (let idx = 0; idx < cycles; idx++) {
+  let newCube = makeCube(size)
+  for (let i = 1; i + 1 < size; i++) {
+    for (let j = 1; j + 1 < size; j++) {
+      for (let k = 1; k + 1 < size; k++) {
+        if (
+          cube[i][j][k] === 1 &&
+          (count(cube, i, j, k) === 2 || count(cube, i, j, k) === 3)
+        ) {
+          newCube[i][j][k] = 1
+        } else {
+          newCube[i][j][k] = 0
+        }
+        if (cube[i][j][k] === 0 && count(cube, i, j, k) === 3)
+          newCube[i][j][k] = 1
+      }
+    }
+  }
+  cube = newCube
+}
+
+console.info(cube.flat(Infinity).reduce(sum))
+
+function sum(acc, cur) {
+  return acc + cur
+}
+function count(cube, z, y, x) {
+  let num = 0
+  for (let i = -1; i < 2; i++) {
+    for (let j = -1; j < 2; j++) {
+      for (let k = -1; k < 2; k++) {
+        num += cube[z + i][y + j][x + k]
+      }
+    }
+  }
+  num -= cube[z][y][x]
+  return num
+}
