2ff4a1c151b9f9e9346a54af7d55f85b91be5523
radulle <radulle@gmail.com>
2021-01-29 02:52:54 +0100

    add 2016-23

---
+++ 2016/23/23a.js
@@ -0,0 +1,60 @@
+const fs = require("fs")
+console.time()
+
+const program = fs
+  .readFileSync("data.txt", "utf8")
+  .trim()
+  .replace(/\r\n/g, "\n")
+  .split("\n")
+  .map((row) => row.split(" "))
+
+const subst = {
+  cpy: "jnz",
+  inc: "dec",
+  dec: "inc",
+  jnz: "cpy",
+  tgl: "inc",
+}
+
+function run(register) {
+  const val = (arg) => (isNaN(+arg) ? register[arg] : +arg)
+  let idx = 0
+  main: while (idx < program.length) {
+    const [cmd, arg1, arg2] = program[idx]
+    switch (cmd) {
+      case "cpy":
+        register[arg2] = val(arg1)
+        idx++
+        break
+      case "inc":
+        register[arg1]++
+        idx++
+        break
+      case "dec":
+        register[arg1]--
+        idx++
+        break
+      case "jnz":
+        if (val(arg1) !== 0) {
+          idx += val(arg2)
+        } else {
+          idx++
+        }
+        break
+      case "tgl":
+        const step = val(arg1)
+        if (step + idx >= 0 && step + idx < program.length) {
+          program[step + idx][0] = subst[program[step + idx][0]]
+        }
+        idx++
+        break
+    }
+  }
+  return register
+}
+
+const resultA = run({ a: 7, b: 0, c: 0, d: 0 }).a
+
+console.info("\nSolution A:")
+console.info(resultA)
+console.timeEnd()
