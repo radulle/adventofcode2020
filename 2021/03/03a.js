dee5c62eee043d05c85b6ab3dc0140374e5f5bf8
radulle <radulle@gmail.com>
2021-12-03 11:55:56 +0100

    add 2021-03

---
+++ 2021/03/03a.js
@@ -0,0 +1,37 @@
+const fs = require("fs");
+
+const consoleTime = (logMe) => {
+  console.time();
+  console.info(logMe());
+  console.timeEnd();
+};
+
+const data = fs
+  .readFileSync("data.txt", "utf8")
+  .split("\n")
+  .map((e) => e.split("").map(Number));
+consoleTime(() => solve(data));
+
+function solve(data) {
+  const gamma = [];
+  for (let i = 0; i < data[0].length; i++) {
+    gamma.push(common(data, i));
+  }
+
+  const epsilon = gamma.map((e) => (e === 1 ? 0 : 1));
+  const result = bin2dec(gamma) * bin2dec(epsilon);
+  return result;
+}
+
+function bin2dec(bin) {
+  return parseInt(bin.join(""), 2);
+}
+
+function common(arr, i) {
+  const half = arr.length / 2;
+  let j = 0;
+  for (const el of arr) {
+    if (el[i] === 1) j++;
+  }
+  return j >= half ? 1 : 0;
+}
---
+++ 2021/03/03b.js
@@ -0,0 +1,43 @@
+const fs = require("fs");
+
+const consoleTime = (logMe) => {
+  console.time();
+  console.info(logMe());
+  console.timeEnd();
+};
+
+const data = fs
+  .readFileSync("data.txt", "utf8")
+  .split("\n")
+  .map((e) => e.split("").map(Number));
+consoleTime(() => solve(data));
+
+function solve(data) {
+  let oxygen = data.slice(),
+    co2scrubber = data.slice(),
+    i = 0;
+  while (oxygen.length > 1) {
+    oxygen = oxygen.filter((e, _, arr) => e[i] === common(arr, i));
+    i++;
+  }
+  i = 0;
+  while (co2scrubber.length > 1) {
+    co2scrubber = co2scrubber.filter((e, _, arr) => e[i] !== common(arr, i));
+    i++;
+  }
+  const result = bin2dec(oxygen[0]) * bin2dec(co2scrubber[0]);
+  return result;
+}
+
+function bin2dec(bin) {
+  return parseInt(bin.join(""), 2);
+}
+
+function common(arr, i) {
+  const half = arr.length / 2;
+  let j = 0;
+  for (const el of arr) {
+    if (el[i] === 1) j++;
+  }
+  return j >= half ? 1 : 0;
+}
