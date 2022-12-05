336bc3d9d87d5f540410a8f01b491c1f6952f3f1
Nikola Radulaški <radulle@gmail.com>
2022-12-05 07:29:35 +0100

    cleanup 2022-05

--- 2022/05/05a.js
+++ 2022/05/05a.js
@@ -1,16 +1,14 @@
 const { consoleTime } = require("lib");
-const { parseData, outputResult } = require("./common");
+const { parseData, outputResult } = require("./cmn");
 
 consoleTime(() => solve());
 
 function solve() {
   const [stacks, instructions] = parseData();
 
-  for (const instruction of instructions) {
-    for (let i = 0; i < instruction[0] + 1; i++) {
-      const a = stacks[instruction[1]].pop();
-      stacks[instruction[2]].push(a);
-    }
+  for (const [count, from, to] of instructions) {
+    const move = stacks[from].splice(-count).reverse();
+    stacks[to].push(...move);
   }
 
   outputResult(stacks);
--- 2022/05/cmn.js
+++ 2022/05/cmn.js
@@ -1,20 +1,18 @@
 const { input } = require("lib");
 
-function parseData() {
+exports.parseData = function parseData() {
   let [_stacks, instructions] = input().split("\n\n");
 
   _stacks = _stacks.split("\n");
-
-  const count = Math.max(
-    ...[..._stacks.pop().matchAll(/\d+/g)].map(([e]) => +e)
-  );
-
   _stacks.reverse();
 
+  const count = Math.max(
+    ...[..._stacks.shift().matchAll(/\d+/g)].map(([e]) => +e)
+  );
   const stacks = new Array(count).fill(null).map((e) => []);
 
   for (let i = 0; i < _stacks.length; i++) {
-    for (let j = 0; j < 9; j++) {
+    for (let j = 0; j < count; j++) {
       const crate = _stacks[i][1 + j * 4];
       if (crate !== " ") stacks[j].push(crate);
     }
@@ -22,18 +20,15 @@ function parseData() {
 
   instructions = instructions
     .split("\n")
-    .map((e) => [...e.matchAll(/\d+/g)].map(([e]) => e - 1));
+    .map((e) => [...e.matchAll(/\d+/g)].map(([e], i) => (i ? e - 1 : +e)));
 
   return [stacks, instructions];
-}
-exports.parseData = parseData;
+};
 
-function outputResult(stacks) {
+exports.outputResult = function outputResult(stacks) {
   let result = "";
   for (const stack of stacks) {
     result += stack.at(-1);
   }
-
   console.info(result);
-}
-exports.outputResult = outputResult;
+};
