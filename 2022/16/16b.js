8711ba9b538a3db9eb23cd8e5e2231b588ad735e
Nikola Radulaški <radulle@gmail.com>
2022-12-17 15:33:04 +0100

    cleanup 2022-16

--- 2022/16/16b.js
+++ 2022/16/16b.js
@@ -12,49 +12,47 @@ function solve() {
     )
     .map((l) => ({ from: l[1], flow: +l[2], to: l[3].split(", ") }));
 
+  const ids = {};
   const dists = {};
   const flows = {};
-  const graph = new Graph();
-  const MINUTES = 26;
 
-  for (const { from, to, flow } of data) {
-    if (!graph.hasNode(from)) graph.addNode(from);
-    for (const t of to) {
-      if (!graph.hasNode(t)) graph.addNode(t);
+  {
+    const graph = new Graph();
+
+    for (const { from, to, flow } of data) {
+      if (!graph.hasNode(from)) graph.addNode(from);
+      for (const t of to) {
+        if (!graph.hasNode(t)) graph.addNode(t);
+      }
+      flows[from] = flow;
     }
-    flows[from] = flow;
-  }
 
-  for (const { from, to, flow } of data) {
-    for (const t of to) {
-      try {
+    for (const { from, to, flow } of data) {
+      for (const t of to) {
         graph.addEdge(from, t, flow);
-      } catch (e) {
-        debugger;
+      }
+    }
+
+    for (const key in flows) {
+      if (flows[key] === 0 && key !== "AA") {
+        delete flows[key];
+        continue;
+      }
+      dists[key] = graph.bfs(key);
+    }
+
+    let i = 0;
+    for (const key in dists) {
+      ids[key] = 2 ** i;
+      i++;
+      for (const [k, d] of dists[key].entries()) {
+        if (k in dists && d !== 0) continue;
+        dists[key].delete(k);
       }
     }
   }
 
-  for (const key in flows) {
-    if (flows[key] === 0 && key !== "AA") {
-      delete flows[key];
-      continue;
-    }
-    dists[key] = graph.bfs(key);
-  }
-
-  for (const key in dists) {
-    for (const [k, d] of dists[key].entries()) {
-      if (k in dists && d !== 0) continue;
-      dists[key].delete(k);
-    }
-  }
-
-  const ids = [...Object.keys(dists)].reduce((a, c, i) => {
-    a[c] = 2 ** i;
-    return a;
-  }, {});
-
+  const MINUTES = 26;
   const totals = new Map();
 
   function explore(to, visited, minute, released) {
