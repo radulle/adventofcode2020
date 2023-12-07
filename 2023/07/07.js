54a18c901ec1f8ce76533c2927300744c442d5bd
Nikola Radulaški <radulle@gmail.com>
2023-12-07 07:45:50 +0100

    simplify 2023-07

--- 2023/07/07.js
+++ 2023/07/07.js
@@ -24,39 +24,23 @@ function parse(input, part) {
 }
 
 function process(data) {
-  data.sort(([a], [b]) => {
-    const typeA = getType(a);
-    const typeB = getType(b);
-    if (typeB > typeA) return 1;
-    if (typeB < typeA) return -1;
-    if (b > a) return 1;
-    if (b < a) return -1;
-    return 0;
-  });
-
-  data.reverse();
+  data.forEach((e, i) => (data[i][0] = getType(e[0])));
+  data.sort(([a], [b]) => (a > b ? 1 : -1));
   return data.reduce((acc, cur, i) => acc + +cur[1] * (i + 1), 0);
 }
 
-function getType(deck) {
+function getType(hand) {
   const map = {};
   let jokers = 0;
 
-  deck.split("").forEach((e) => {
+  hand.split("").forEach((e) => {
     if (e === "0") jokers++;
     else if (map[e]) map[e]++;
     else map[e] = 1;
   });
 
-  if (jokers === 5) return 9;
-
-  const values = Object.values(map).sort((a, b) => b - a);
-  values[0] = values[0] + jokers;
-  if (values[0] === 5) return 9;
-  if (values[0] === 4) return 8;
-  if (values[0] === 3 && values[1] === 2) return 7;
-  if (values[0] === 3) return 6;
-  if (values[0] === 2 && values[1] === 2) return 5;
-  if (values[0] === 2) return 4;
-  if (values[0] === 1) return 3;
+  if (jokers === 5) return 5 + hand;
+  const str = Object.values(map).sort((a, b) => b - a);
+  str[0] = str[0] + jokers;
+  return str.join("") + hand;
 }
