65d9401069fa86e2ed2b5040538fa4239823ced6
Nikola Radulaški <radulle@gmail.com>
2023-12-12 15:29:53 +0100

    simplify 2023-12

--- 2023/12/12.js
+++ 2023/12/12.js
@@ -7,14 +7,13 @@ function solve(input) {
 }
 
 function parse(input, m) {
-  input = input.split("\n").map((line) => line.split(" "));
-  for (let i = 0; i < input.length; i++) {
-    input[i][0] = Array(m).fill(input[i][0]).join("?").split("");
-    input[i][1] = Array(m)
-      .fill(input[i][1].split(",").map((e) => +e))
-      .flat();
-  }
-  return input;
+  return input
+    .split("\n")
+    .map((row) => row.split(" "))
+    .map(([c, g]) => [
+      Array(m).fill(c).join("?"),
+      Array(m).fill(g.split(",").map(Number)).flat(),
+    ]);
 }
 
 function process(data) {
@@ -30,13 +29,12 @@ function comb(cache, c, g, ci, gi, ggi) {
     return 0;
   }
   let ans = 0;
-  for (const e of [".", "#"]) {
-    if (c[ci] === e || c[ci] === "?") {
-      if (e === "#") ans += comb(cache, c, g, ci + 1, gi, ggi + 1);
-      if (e === "." && ggi === 0) ans += comb(cache, c, g, ci + 1, gi, 0);
-      if (e === "." && ggi > 0 && gi < g.length && g[gi] === ggi)
-        ans += comb(cache, c, g, ci + 1, gi + 1, 0);
-    }
+  if (c[ci] === "#" || c[ci] === "?")
+    ans += comb(cache, c, g, ci + 1, gi, ggi + 1);
+  if (c[ci] === "." || c[ci] === "?") {
+    if (ggi === 0) ans += comb(cache, c, g, ci + 1, gi, 0);
+    if (ggi > 0 && gi < g.length && g[gi] === ggi)
+      ans += comb(cache, c, g, ci + 1, gi + 1, 0);
   }
   return (cache[key] = ans);
 }
