690252e9546ad9e3b7c368b73ee4af305ae5301e
Nikola Radulaški <radulle@gmail.com>
2023-12-16 07:16:11 +0100

    add 2023 template

---
+++ 2023/template/xx.js
@@ -0,0 +1,15 @@
+const { input, consoleTime } = require("lib");
+
+consoleTime(() => solve(input()));
+
+function solve(input) {
+  return process(parse(input));
+}
+
+function parse(input) {
+  return "TODO: data";
+}
+
+function process(data) {
+  return "TODO: result";
+}
