a151f51d9c35496daf4f825d40257d39d028e782
Nikola Radulaški <radulle@gmail.com>
2024-12-11 22:37:16 +0100

    format 2024-03

--- 2024/03/03a.js
+++ 2024/03/03a.js
@@ -7,16 +7,16 @@ function solve(input) {
 }
 
 function parse(input) {
-  const matches = input.match(/mul\(\d+,\d+\)/g)
+  const matches = input.match(/mul\(\d+,\d+\)/g);
   return matches;
 }
 
 function process(matches) {
-  let sum = 0
+  let sum = 0;
   for (const match of matches) {
-    const [_, a, b] = match.match(/mul\((\d+),(\d+)\)/)
-    sum += a * b
+    const [_, a, b] = match.match(/mul\((\d+),(\d+)\)/);
+    sum += a * b;
   }
 
-  return sum
+  return sum;
 }
--- 2024/03/03b.js
+++ 2024/03/03b.js
@@ -7,26 +7,26 @@ function solve(input) {
 }
 
 function parse(input) {
-  const matches = input.match(/(mul)\((\d+),(\d+)\)|do\(\)|don't\(\)/g)
+  const matches = input.match(/(mul)\((\d+),(\d+)\)|do\(\)|don't\(\)/g);
   return matches;
 }
 
 function process(matches) {
   let process = true;
-  let sum = 0
+  let sum = 0;
   for (const match of matches) {
     if (match === "do()") {
       process = true;
-      continue
+      continue;
     }
     if (match === "don't()") {
       process = false;
-      continue
+      continue;
     }
-    if (!process) continue
-    const [_, a, b] = match.match(/mul\((\d+),(\d+)\)/)
-    sum += a * b
+    if (!process) continue;
+    const [_, a, b] = match.match(/mul\((\d+),(\d+)\)/);
+    sum += a * b;
   }
 
-  return sum
+  return sum;
 }
