f8a910a988c441f244408346671bb3fdd86339ce
Nikola Radulaški <radulle@gmail.com>
2024-12-11 22:37:05 +0100

    format 2024-02

--- 2024/02/02a.js
+++ 2024/02/02a.js
@@ -7,25 +7,25 @@ function solve(input) {
 }
 
 function parse(input) {
-  const reports = input.split("\n").map(r => r.split(" ").map(Number))
-  return reports
+  const reports = input.split("\n").map((r) => r.split(" ").map(Number));
+  return reports;
 }
 
 function process(reports) {
   let safe = [];
 
   reports: for (const report of reports) {
-    const asc = report[1] > report[0]
+    const asc = report[1] > report[0];
 
     for (let i = 1; i < report.length; i++) {
       if (Math.abs(report[i] - report[i - 1]) > 3) continue reports;
-      if (report[i] === report[i - 1]) continue reports
-      if (asc && report[i] < report[i - 1]) continue reports
-      if (!asc && report[i] > report[i - 1]) continue reports
+      if (report[i] === report[i - 1]) continue reports;
+      if (asc && report[i] < report[i - 1]) continue reports;
+      if (!asc && report[i] > report[i - 1]) continue reports;
     }
 
-    safe.push(report)
+    safe.push(report);
   }
 
-  return safe.length
+  return safe.length;
 }
--- 2024/02/02b.js
+++ 2024/02/02b.js
@@ -7,8 +7,8 @@ function solve(input) {
 }
 
 function parse(input) {
-  const reports = input.split("\n").map(r => r.split(" ").map(Number))
-  return reports
+  const reports = input.split("\n").map((r) => r.split(" ").map(Number));
+  return reports;
 }
 
 function process(reports) {
@@ -16,18 +16,18 @@ function process(reports) {
 
   reports: for (const report of reports) {
     nReport: for (let i = 0; i < report.length; i++) {
-      const nReport = report.filter((_, k) => k !== i)
-      const asc = nReport[1] > nReport[0]
+      const nReport = report.filter((_, k) => k !== i);
+      const asc = nReport[1] > nReport[0];
       for (let j = 1; j < nReport.length; j++) {
         if (Math.abs(nReport[j] - nReport[j - 1]) > 3) continue nReport;
-        if (nReport[j] === nReport[j - 1]) continue nReport
-        if (asc && nReport[j] < nReport[j - 1]) continue nReport
-        if (!asc && nReport[j] > nReport[j - 1]) continue nReport
+        if (nReport[j] === nReport[j - 1]) continue nReport;
+        if (asc && nReport[j] < nReport[j - 1]) continue nReport;
+        if (!asc && nReport[j] > nReport[j - 1]) continue nReport;
       }
-      safe.push(report)
-      continue reports
+      safe.push(report);
+      continue reports;
     }
   }
 
-  return safe.length
+  return safe.length;
 }
