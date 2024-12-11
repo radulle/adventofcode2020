78492000943272460b550ee4d9b677594d4e45c7
Nikola Radulaški <radulle@gmail.com>
2024-12-11 22:07:32 +0100

    refactor 2024-08

--- 2024/08/08.js
+++ 2024/08/08.js
@@ -3,7 +3,7 @@ const { input, consoleTime } = require("lib");
 consoleTime(() => solve(input()));
 
 function solve(input) {
-  return [process(...parse(input))];
+  return [process(...parse(input), 0), process(...parse(input), 1)];
 }
 
 function parse(input) {
@@ -14,8 +14,8 @@ function parse(input) {
 
   for (let i = 0; i < input.length; i++) {
     for (let j = 0; j < input[i].length; j++) {
-      if (input[i][j] === ".") continue
-      if (input[i][j] === "#") continue
+      if (input[i][j] === ".") continue;
+      if (input[i][j] === "#") continue;
       const v = input[i][j];
       if (!map[v]) map[v] = [];
       map[v].push([i, j]);
@@ -25,25 +25,23 @@ function parse(input) {
   return [map, rmax, cmax];
 }
 
-function process(map, rmax, cmax) {
-  console.info(rmax, cmax)
-  const nMap = new Array(rmax).fill(null).map(e => new Array(cmax).fill(0));
+function process(map, rmax, cmax, part) {
+  const grid = new Array(rmax).fill(null).map(() => new Array(cmax).fill(0));
   for (const ant in map) {
-    // go through each pair of items
     for (let i = 0; i < map[ant].length; i++) {
       for (let j = 0; j < map[ant].length; j++) {
         if (i === j) continue;
         const [r1, c1] = map[ant][i];
         const [r2, c2] = map[ant][j];
-        nMap[r1][c1] = 1;
+        if (part) grid[r1][c1] = 1;
 
         let d = 2;
         while (true) {
-          const nr1 = d * r1 - (d - 1) * r2
-          const nc1 = d * c1 - (d - 1) * c2
-          // console.info(nr1, nc1)
-          if ((nr1 < 0 || nr1 >= rmax || nc1 < 0 || nc1 >= cmax)) break;
-          nMap[nr1][nc1] = 1;
+          const nr1 = d * r1 - (d - 1) * r2;
+          const nc1 = d * c1 - (d - 1) * c2;
+          if (nr1 < 0 || nr1 >= rmax || nc1 < 0 || nc1 >= cmax) break;
+          grid[nr1][nc1] = 1;
+          if (!part) break;
           d++;
         }
 
@@ -51,17 +49,17 @@ function process(map, rmax, cmax) {
         while (true) {
           const nr2 = d * r2 - (d - 1) * r1;
           const nc2 = d * c2 - (d - 1) * c1;
-          // console.info(nr2, nc2)
-          if ((nr2 < 0 || nr2 >= rmax || nc2 < 0 || nc2 >= cmax)) break
-          nMap[nr2][nc2] = 1;
-          d++
+          if (nr2 < 0 || nr2 >= rmax || nc2 < 0 || nc2 >= cmax) break;
+          grid[nr2][nc2] = 1;
+          if (!part) break;
+          d++;
         }
       }
     }
   }
-  console.info(nMap.map((row) => row.map(e => e === 0 ? " " : e).join("")).join("\n"));
-  console.info(nMap.reduce((acc, row) => acc + row.reduce((acc, cell) => acc + cell, 0), 0));
-}
 
-// 1187 too low
-// 1264 high
+  return grid.reduce(
+    (acc, row) => acc + row.reduce((acc, cell) => acc + cell, 0),
+    0
+  );
+}
